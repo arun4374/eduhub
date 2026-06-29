cfab5830e52d4d92f4ac21a91023211bfc3ccfe0
arun4374 <takeintamila@gmail.com>
2026-06-29 08:15:24 +0530

    fix img bug

--- components/home/MobileAppSection.tsx
+++ components/home/MobileAppSection.tsx
@@ -80,7 +80,7 @@ export function MobileAppSection() {
               ))}
             </ul>
 
-            <div className="flex items-center justify-center md:justify-start gap-4 h-14">
+            <div className="flex items-center justify-center md:justify-start gap-4">
               {!showInterestModule ? (
                 <div className="relative group">
                   <a 
@@ -92,14 +92,13 @@ export function MobileAppSection() {
                     aria-label="Get it on Google Play" 
                     className="inline-block transition-transform hover:scale-105 duration-300"
                   >
-                    <div className="scale-[0.8] origin-bottom">
-                      <Image
-                        src="/App/Google_Play_Badge.png"
-                        alt="Get it on Google Play"
-                        width={202}
-                        height={50}
-                        priority
-                      />
+                    <div className="h-[100px] flex items-center">
+                        <Image
+                            src="/App/Google_Play_Badge.png"
+                            alt="Get it on Google Play"
+                            width={202}
+                            height={50}
+                            priority />
                     </div>
                   </a>
                   <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-1 w-max opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
